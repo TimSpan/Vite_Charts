fd1db371a46c3be56bdd0c292a7694f44ecbd437
TimSpan <cat20201314@163.com>
2023-06-02 17:19:22 +0800

    frist

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Charts from './components/charts.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <!-- <HelloWorld  /> -->
+  <Charts />
+</template>
+
+<style scoped>
+
+</style>
